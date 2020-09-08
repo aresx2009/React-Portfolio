403be06c3efdd83ca4038278fc52fe800615f912
Walter Kieke <walterkieke@Walters-MBP.attlocal.net>
2020-09-08 07:08:37 -0500

    Home page completed

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ function App() {
   return (
     <div className="demo-big-content">
       <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title="Walter Kieke" scroll>
           <Navigation>
+            <Link to="/">Home</Link>
             <Link to="/resume">Resume</Link>
             <Link to="/aboutme">About Me</Link>
             <Link to="/projects">Project</Link>
             <Link to="/contact">Contact</Link>
           </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title="Walter Kieke">
           <Navigation>
+            <Link to="/">Home</Link>
             <Link to="/resume">Resume</Link>
             <Link to="/aboutme">About Me</Link>
             <Link to="/projects">Project</Link>

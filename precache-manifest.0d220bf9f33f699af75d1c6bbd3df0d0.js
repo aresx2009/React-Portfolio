11e6ee697129420afe76c9f6fc9f31e335b4157c
Walter Kieke <walterkieke@Walters-MBP.attlocal.net>
2020-09-15 19:46:15 -0500

    Updates

---
+++ precache-manifest.0d220bf9f33f699af75d1c6bbd3df0d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d35f29adfdc3abc6a9792d00850b4f",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "78ba3f537505d906ab7f",
+    "url": "/React-Portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "75cba086d61fef227964",
+    "url": "/React-Portfolio/static/css/main.d3b8b9cd.chunk.css"
+  },
+  {
+    "revision": "78ba3f537505d906ab7f",
+    "url": "/React-Portfolio/static/js/2.d8b1e45d.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/React-Portfolio/static/js/2.d8b1e45d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cba086d61fef227964",
+    "url": "/React-Portfolio/static/js/main.14976731.chunk.js"
+  },
+  {
+    "revision": "89f5ed8e982227547873",
+    "url": "/React-Portfolio/static/js/runtime-main.3435375c.js"
+  },
+  {
+    "revision": "7b14a867d3eec15734635d89ca8c30fd",
+    "url": "/React-Portfolio/static/media/Walter.7b14a867.pdf"
+  },
+  {
+    "revision": "9d8c1c28455e8ddae47e5d75458b5163",
+    "url": "/React-Portfolio/static/media/profile.9d8c1c28.JPG"
+  }
+]);
